0570b72c252f82172970ccdba01015ed13ccc795
Tyler Chen <tyche4490@gmail.com>
2023-03-08 14:26:15 -0800

    add barometric code

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,10 +2,36 @@
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
 
-void setup() {
+Adafruit_BMP280 bmp;
 
+void setup() {
+  Serial.begin(9600);
+  unsigned status = bmp.begin(0x76, 0x58);
+  if (!status) {
+    Serial.println("No BMP sensor found, halting,,,");
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    while (true) delay(100); // stop program
+  }
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void loop() {
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
 
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
 }
