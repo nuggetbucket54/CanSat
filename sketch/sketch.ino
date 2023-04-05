402a37cf848d8c4aa98af07bf4907707c53911c2
Tyler Chen <tyche4490@gmail.com>
2023-04-05 09:19:44 -0700

    some lora work, still non-functional

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -9,14 +9,21 @@
 #define gpsRX 4
 #define gpsTX 3
 #define gpsBaud 9600
+
+#define loraRX 8
+#define loraTX 9
+#define loraBaud 115200
+
+int statusLED = 2;
+
 TinyGPSPlus gps;
 
 Adafruit_BMP280 bmp;
 File dataFile = SD.open("data.txt", FILE_WRITE);
-int statusLED = 2;
 int flash=1;
 
 SoftwareSerial gpsModule(gpsRX, gpsTX);
+SoftwareSerial loraModule(loraRX, loraTX);
 
 // data variables
 float temp, pressure, altitude;
@@ -24,12 +31,18 @@ double latitude, longitude;
 TinyGPSDate date;
 TinyGPSTime time;
 
+char dataSep[] = ",";
+char dataStorage[50];
+char messageBuf[50];
+char dtosbuf[16];
+
 void setup() {
   Serial.begin(115200);
   pinMode(statusLED, OUTPUT);
   digitalWrite(statusLED, HIGH);
   gpsModule.begin(gpsBaud);
-  unsigned status = bmp.begin(0x76, 0x58);
+  loraModule.begin(loraBaud);
+  unsigned status = bmp.begin(0x76);
   if (!status) {
     Serial.println("No BMP sensor found, halting,,,");
     Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
@@ -52,23 +65,33 @@ void loop() {
 
   digitalWrite(statusLED, flash);
   flash ^= 1;
-  
+  strcpy(dataStorage, dataSep);
   temp = bmp.readTemperature();
   pressure = bmp.readPressure();
   altitude = bmp.readAltitude(1013.25);
+  addData(temp, 2, true);
+  addData(pressure, 2, true);
+  addData(altitude, 2, false);
+  
 
   if (gps.location.isValid()) {
-    latitude = gps.location.lat();    
+    latitude = gps.location.lat();  
+    strcat(dataStorage, dataSep);
+    addData(latitude, 6, true);
+      
     Serial.print(F("Latitude: "));
-    Serial.println(latitude, 8);
+    Serial.println(latitude, 6);
 
     longitude = gps.location.lng(); 
+    addData(longitude, 8, false);
     Serial.print(F("Longitude: "));
-    Serial.println(longitude, 8);
+    Serial.println(longitude, 6);
     
   }
 
-  
+  loraModule.listen();
+  loraModule.println("AT+SEND=0,5,HELLO");
+  gpsModule.listen();
   
   Serial.print(F("Temperature = "));
   Serial.print(temp);
@@ -82,12 +105,25 @@ void loop() {
   Serial.print(altitude); 
 
   Serial.println();
+  Serial.println(dataStorage);
+
+  sprintf(dtosbuf, "%d", strlen(dataStorage)-1);
+  //strcpy(messageBuf, "AT+SEND=0,");
+  //strcpy(messageBuf, dataSep);
+
   
   smartDelay(500);
 }
   
 
-
+static void addData(double val, int precis, bool sep) {
+  dtosbuf[0] = '\0';
+  dtostrf(val, 1, precis, dtosbuf);
+  Serial.println(dtosbuf);
+  strcat(dataStorage, dtosbuf);
+  if (sep)
+    strcat(dataStorage, dataSep);    
+}
 
 // This custom version of delay() ensures that the gps object
 // is being "fed".
