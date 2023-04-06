522fbaf7698b89f3a1fdcad259f92ea8ad27a716
Tyler Chen <tyche4490@gmail.com>
2023-04-06 09:43:04 -0700

    data separation now functional

--- groundstation/groundstation.ino
+++ groundstation/groundstation.ino
@@ -1,11 +1,17 @@
 
 #include <SoftwareSerial.h>
  
+
+
 SoftwareSerial lora(9, 8);
 
 char loraBuffer[80];
 int bufferPos = 0;
 int clock = 0;
+char temp[7], pressure[10], altitude[10];
+char lat[13], lng[13]; // 1 byte for sign/3 for degree/8 for precision
+const char dataSep[] = ",";
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -33,22 +39,32 @@ void flushLora() {
 
     loraBuffer[bufferPos] = k;
     bufferPos ++;
+  
     if (k=='\n') {
       loraBuffer[bufferPos-1] = '\0';
       Serial.println(loraBuffer);
+      parseData();
       bufferPos = 0;
     }
-  
-
   }  
-
 }
 
-static void smartDelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do 
-  {
-    flushLora();
-  } while (millis() - start < ms);
+void parseData() {
+  int index=0;
+  char *token = strtok(loraBuffer, dataSep);
+
+  while (token != NULL) {
+    switch (index) {
+      case 2:
+        Serial.print(F("Temperature: "));
+        break;
+      case 3:
+        Serial.print(F("Pressure: "));
+        break;
+    }
+    index++;
+    Serial.println(token);
+    token = strtok(NULL, dataSep);
+  }
+
 }
