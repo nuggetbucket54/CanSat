2785f30c8a2ec9ea5b67b8de0b119bbfb01b9f7a
Ryan <rcheng888446@gmail.com>
2023-04-22 04:28:17 -0600

    added data recording

---
+++ assign.py
@@ -0,0 +1,37 @@
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook("data.xlsx")
+worksheet = workbook.add_worksheet()
+
+worksheet.write("A1", "Temperature (°C)")
+worksheet.write("B1", "Pressure (Pa)")
+worksheet.write("C1", "Seismometer (gal)")
+worksheet.write("D1", "GPS Latitude")
+worksheet.write("E1", "GPS Longitude")
+worksheet.write("F1", "Time (ms)")
+worksheet.write("G1", "Seismometer Time (ms)")
+
+with open("test.txt", "r") as f:
+    lines = f.readlines()
+
+row = 2
+seismorow = 2
+
+for l in lines:
+    data = l.split(",")
+
+    worksheet.write(f"A{row}", data[2]) # Temperature
+    worksheet.write(f"B{row}", data[3]) # Pressure
+
+    for i in range(1, int(data[-7]) + 1): 
+        worksheet.write(f"C{seismorow}", data[3 + i]) # Seismometer
+        worksheet.write(f"G{seismorow}", float(data[-6]) + (float(data[-5]) * i)) # Seismometer Time
+        seismorow += 1
+
+    worksheet.write(f"D{row}", data[-4]) # GPS Latitude
+    worksheet.write(f"E{row}", data[-3]) # GPS Longitude
+    worksheet.write(f"F{row}", data[-6]) # Time
+    row += 1
+    
+
+workbook.close()
--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import serial
 import io
-import xlsxwriter
 import sys
 from queue import Queue
 import time
@@ -8,6 +7,7 @@ import time
 print(sys.argv)
 queued = Queue()
 start_time = time.time()
+
 
 if len(sys.argv) < 2:
     print("Usage: python3 script.py <port> <address=5656> <networkID=12> <passphrase=none>. Replacing a field with D will use the default value.")
@@ -30,54 +30,43 @@ sio.flush()
 queued.put(f"AT+ADDRESS={address}\r\n")
 queued.put("AT+PARAMETER=12,4,1,7\r\n")
 
-workbook = xlsxwriter.Workbook("data.xlsx")
-worksheet = workbook.add_worksheet()
-
-worksheet.clear()
-worksheet.write("A1", "Temperature (°C)")
-worksheet.write("B1", "Pressure (Pa)")
-worksheet.write("C1", "Seismometer (g)")
-worksheet.write("D1", "GPS Latitude")
-worksheet.write("E1", "GPS Longitude")
-worksheet.write("F1", "Time (ms)")
-worksheet.write("G1", "Seismometer Time (ms)")
-
-row = 2
-seismorow = 2
-
 ready = False
-while True:
-    #reading the data (output of arduino sketch)
-    data = ser.readline()
-    data = data.decode()
-    data = data.strip()
-    if data:
-        ready = True
-        end_time = time.time()
-        elapsed_time = (end_time - start_time) * 1000 # in ms
-        start_time = end_time
-        print(data)
+tflag = True
+with open('data.txt', 'w') as f:
+    while True:
+        #reading the data (output of arduino sketch)
+        data = ser.readline()
+        data = data.decode()
+        data = data.strip()
 
-        data = data.split(",")
-        nums = len(data) - 8
+        if not data.startswith("+RCV"):
+            pass
+        else:
+            if data:
+                ready = True
+                end_time = time.time()
+                elapsed_time = (end_time - start_time) * 1000 # in ms
+                if tflag:
+                    timediff = elapsed_time
+                    last_time = end_time
+                    tflag = False
+                else:
+                    timediff = (end_time - last_time) * 1000
+                    last_time = end_time
 
-        worksheet.write(f"A{row}", data[2]) # Temperature
-        worksheet.write(f"B{row}", data[3]) # Pressure
+                print(data)
 
-        for i in range(1, nums + 1): # Seismometer
-            worksheet.write(f"C{seismorow}", data[3 + i])
-            worksheet.write(f"G{seismorow}", elapsed_time * i / nums)
-            seismorow += 1
-
-        worksheet.write(f"D{row}", data[-4]) # GPS Latitude
-        worksheet.write(f"E{row}", data[-3]) # GPS Longitude
-        worksheet.write(f"F{row}", elapsed_time) # Time
-        row += 1
-        
-    # if received data from other radio   
-    if ready and not queued.empty():
-        msg = queued.get()
-        sio.write(msg)
-        sio.flush()
-        print("send", msg)
-        ready = False
+                data = data.split(",")
+                num = len(data)-8
+                data.insert(-4, str(num))
+                data.insert(-4, str(elapsed_time))
+                data.insert(-4, str(timediff / num))
+                f.write(",".join(data) + "\n")
+                
+            # if received data from other radio   
+            if ready and not queued.empty():
+                msg = queued.get()
+                sio.write(msg)
+                sio.flush()
+                print("send", msg)
+                ready = False
